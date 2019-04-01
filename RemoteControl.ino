1cd35161f77c58464ef16e018bf2a530d4fb8f67
fangy51 <45600821+fangy51@users.noreply.github.com>
2019-03-31 21:40:39 -0400

    Update RemoteControl.ino

--- RemoteControl.ino
+++ RemoteControl.ino
@@ -8,9 +8,9 @@ int led3 = 3
 IRrecv irrecv(REC_PIN);
 decode_results results;
 int itsONled[] = {0,0,0,0};
-    #define code1  33772 // code received from button A
-    #define code2  52972 // code received from button B
-    #define code3  3494 // code received from button C
+    #define code1  FF30CF // code received from button 1
+    #define code2  FF18E7 // code received from button 2
+    #define code3  FF7A85 // code received from button 3
 void setup()
 {
   Serial.begin(9600);   
@@ -24,7 +24,7 @@ void loop() {
   if (irrecv.decode(&results)) {
     unsigned int value = results.value;
     switch(value) {
-       case code1: // push button A to let led1 on/off
+       case code1: // push button 1 to let led1 on/off
             if(itsONled[1] == 1) {        
                 digitalWrite(led1, LOW);   
                 itsONled[1] = 0;          
@@ -34,7 +34,7 @@ void loop() {
              }
               break;
           
-       case code2: // push button B to let led2 on/off
+       case code2: // push button 2 to let led2 on/off
             if(itsONled[2] == 1) {        
                 digitalWrite(led1, LOW);   
                 itsONled[2] = 0;          
@@ -44,7 +44,7 @@ void loop() {
              }
               break;
               
-              case code3: // push button C to let led3 on/OFF
+              case code3: // push button 3 to let led3 on/OFF
             if(itsONled[3] == 1) {        
                 digitalWrite(led1, LOW);   
                 itsONled[3] = 0;          
